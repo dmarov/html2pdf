f09a98813c2f92b56009a6dfbb9c222558b65b7a
Dmitry Marov <d.marov94@gmail.com>
2018-05-29 17:02:30 +0300

    working state

--- index.js
+++ index.js
@@ -1,14 +1,78 @@
 #!/usr/bin/env node
+const Koa = require('koa');
+const Router = require('koa-router');
 const puppeteer = require('puppeteer');
 
+const app = new Koa();
 
-(async () => {
-  const browser = await puppeteer.launch({
-  executablePath: '/usr/local/bin/chrome',
+
+let router = new Router();
+
+router.get('/html-to-pdf', async (ctx, next) => {
+
+    try {
+
+        let query = ctx.request.query;
+        let href = query.href;
+
+        if (!href) {
+
+            ctx.response.status = 404;
+            ctx.response.body = 'reference to document was not specified';
+            return;
+
+        }
+
+        let name = query.name ? query.name : 'response.pdf';
+        let scale = query.scale ? query.scale : 1;
+        let format = 'A4';
+        let width = '';
+        let height = '';
+        let margin = {
+            top: query.top ? query.top : 0,
+            bottom: query.bottom ? query.bottom : 0,
+            left: query.left ? query.left : 0,
+            right: query.right ? query.right : 0,
+        }
+
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/local/bin/chrome',
+        });
+
+        let params = {
+            scale: scale,
+            margin: margin,
+        };
+
+        if (width && height) {
+
+            params['width'] = width;
+            params['height'] = height;
+
+        } else {
+
+            params['format'] = format;
+        }
+
+
+        const page = await browser.newPage();
+        await page.goto(href, {waitUntil: 'networkidle2'});
+        let buf = await page.pdf(params);
+        await browser.close();
+
+        ctx.set("Content-Type", "application/octet-stream");
+        ctx.set("Content-Disposition", `attachment; filename="${name}"`);
+        ctx.response.body = buf;
+
+    } catch (e) {
+
+        ctx.response.status = 500;
+
+    }
+
 });
-  const page = await browser.newPage();
-  await page.goto('https://google.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'result.pdf', format: 'A4'});
 
-  await browser.close();
-})();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
