ba1616c01fb545159fa19b3715898d0c4e9a7a62
Dmitry Marov <d.marov94@gmail.com>
2018-05-30 17:22:34 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 const Koa = require('koa');
 const Router = require('koa-better-router');
+const kjwt = require('koa-jwt');
+const fs = require('fs');
+const path = require('path');
 
 const apiV1 = require('./api/v1');
 const args = require('./lib/args');
 
 const app = new Koa();
 
-let apiV1Router = Router({ prefix: '/api/v1' });
-apiV1Router.extend(apiV1);
+let publicKey = fs.readFileSync(path.resolve(args.publicKey));
+app.use(kjwt({ secret: publicKey }));
+
+let apiV1Full = Router({ prefix: '/api/v1' });
+apiV1Full.extend(apiV1);
 
 app.use(apiV1.middleware());
-app.use(apiV1Router.middleware());
+app.use(apiV1Full.middleware());
 
 app.listen(args.port);
